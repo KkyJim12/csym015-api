c85853e9600bdddb9f0d44a010eeeec0b63c5ada
Jimmynmk <jirakarnjim1@gmail.com>
2022-09-07 12:02:56 +0700

    Get api success

--- app.js
+++ app.js
@@ -10,21 +10,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post("/api/geocoding", async (req, res) => {
+  try {
+    let geo = [];
+
+    for (let i = 0; i < req.body.destinations.length; i++) {
+      let response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.destinations[i]}&key=AIzaSyAMGJ-1QIKCEJ99HPGuVxJdBK146vE0M-M`
+      );
+      geo.push({
+        name: req.body.destinations[i],
+        location: response.data.results[0].geometry.location,
+      });
+    }
+
+    res.status(200).send({ data: geo });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.post("/api/destinations", async (req, res) => {
   try {
-    console.log(req.body.message);
-    let response = await axios.get(
-      `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=Northampton&origins=London&key=${process.env.API_KEY}`
-    );
+    let results = [];
 
-    let destinations = [
-      response.data.destination_addresses[0],
-      response.data.origin_addresses[0],
-    ];
+    for (let i = 0; i < req.body.destinations.length; i++) {
+      let response = await axios.get(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${req.body.destinations[i].location.lat}&lon=${req.body.destinations[i].location.lng}&appid=7ed43b13cc6dabfebe659dca2125f447`
+      );
+      results.push({
+        name: req.body.destinations[i].name,
+        data: response.data,
+      });
+    }
 
-    console.log(destinations);
-
-    res.status(200).json({ data: response, message: "success" });
+    res.status(200).json({ data: results, message: "success" });
   } catch (error) {
     console.log(error);
   }
